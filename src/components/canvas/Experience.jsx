d5030e6aecc163034f48be6bcc221b646b1e6189
FellowMustard <rachmawanng33@gmail.com>
2025-09-05 16:54:39 +0700

    fixing color and adding lamp material

--- src/components/canvas/Experience.jsx
+++ src/components/canvas/Experience.jsx
@@ -1,30 +1,23 @@
-import { Canvas } from "@react-three/fiber"
-import Light from "./Light"
-import { OrbitControls } from "@react-three/drei"
-import { Room } from "./Room"
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Room } from "./Room";
 
-function Experience(){
-    return(
-        <Canvas
-            gl={{ antialias: true}}
-            camera={{ position: [5, 8, 5], fov: 50 }}
-        >
-            <Light/>
-            <Room position={[0,-1,0]}/>
-            <OrbitControls
-                // Horizontal (azimuth)
-                minAzimuthAngle={0}             
-                maxAzimuthAngle={Math.PI / 2} 
-
-                // Vertical (polar)
-                minPolarAngle={Math.PI / 6} 
-                maxPolarAngle={Math.PI / 2}    
-
-                enableZoom={true}
-                enablePan={false}
-                />
-        </Canvas>
-    )
+function Experience() {
+  return (
+    <Canvas gl={{ antialias: true }} camera={{ position: [5, 8, 5], fov: 50 }}>
+      <Room position={[0, -1, 0]} />
+      <OrbitControls
+        // Horizontal (azimuth)
+        minAzimuthAngle={0}
+        maxAzimuthAngle={Math.PI / 2}
+        // Vertical (polar)
+        minPolarAngle={Math.PI / 6}
+        maxPolarAngle={Math.PI / 2}
+        enableZoom={true}
+        enablePan={false}
+      />
+    </Canvas>
+  );
 }
 
-export default Experience
+export default Experience;
